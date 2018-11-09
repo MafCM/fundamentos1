8e475b6306e069c4b1e65f230ecf07a807fdfe41
Maf CM <mf_b_j_w_mc@hotmail.com>
2018-11-09 14:28:14 -0600

    maquetación y jsintro

---
+++ JS.js
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en" dir="ltr">
+<head>
+</head>
+<body>
+</body>
+<script type="text/javascript">
+
+document.write("Estoy escrito con javascript")
+
+</script>
+
+<ul>
+ <li>Metodo</li>
+ <li>String o Texto= siempre entre comillas</li>
+ <li>variable</li>
+</ul>
